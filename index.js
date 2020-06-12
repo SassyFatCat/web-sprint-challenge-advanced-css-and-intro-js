a757571e910d2d4d4337e9dd3f8d71047793d7cf
Ryan Lee <therealsassyfatcat@outlook.com>
2020-06-12 11:35:32 -0500

    almost done

--- index.js
+++ index.js
@@ -208,11 +208,13 @@ const artists = [
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
 
-
+console.log(artists[0].name);
+console.log(artists[2].name);
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
-
+artists[8].name = "Vincent Van Gogh";
+console.log(artists[8].name);
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
  *     (1) artists array
@@ -223,18 +225,18 @@ const artists = [
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
 function getArtistByIndex(array, index) {
-    /* code here */
-  }
+    return `The artist at index ${index} is ${array[index].name}`
+  };
   
   /**
 
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who lived in the 20th century (1900-2000) */
 
-function get20s(/* Code here */){
-
-  /* Code here */
-
+function get20s(arr){
+let livedIn20Century = [];
+arr.forEach(element => element.years.split(' ')[2] >= 1900 && element.years.split(' ')[0] >= 1900 && element.years.split(' ')[0] < 2000 ? livedIn20Century.push(element) : null);
+return livedIn20Century;
 }
 
 
@@ -248,8 +250,9 @@ function get20s(/* Code here */){
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
 */
-function removeArtist(/*code here*/) {
-    /* code here */
+function removeArtist(arr, index) {
+    arr.splice(index, 1);
+    console.log(arr.length);
   }
   
   /**
@@ -269,9 +272,17 @@ bio: Add 1-2 sentences (or use lorem ipsum)
 
 At the end, this function should console.log() the new array with information added"*/
 
-function addArtist(/* Code here */){
+function addArtist(arr, id, name, years, genre, nationality, bio){
 
-    /* Code here */
+    arr.push({
+      id,
+      name,
+      years,
+      genre,
+      nationality,
+      bio
+    });
+    console.log(arr)
 
   }
 
